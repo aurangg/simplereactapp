fe7473766300e84b932142f25dd7f7043dbaa59b
Aurang <aurangzebbbk@gmail.com>
2019-05-25 10:48:04 +0500

    Updates

---
+++ precache-manifest.b67f5a3b77440aa56cd0571fc3b62909.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6bff688f2dbbbf65c46fd3e30a5ca24",
+    "url": "/R/index.html"
+  },
+  {
+    "revision": "4412503dc5178f7821dd",
+    "url": "/R/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "87ba338c3f96dafa7d32",
+    "url": "/R/static/css/main.81cab1cb.chunk.css"
+  },
+  {
+    "revision": "4412503dc5178f7821dd",
+    "url": "/R/static/js/2.82b9c808.chunk.js"
+  },
+  {
+    "revision": "87ba338c3f96dafa7d32",
+    "url": "/R/static/js/main.6ba3eab6.chunk.js"
+  },
+  {
+    "revision": "29739b707a740ccd2ffb",
+    "url": "/R/static/js/runtime~main.dbb35d15.js"
+  }
+]);
